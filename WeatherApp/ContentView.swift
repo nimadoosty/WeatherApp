3c986cc19142c2dab4c98240437716808b4c40bd
Nima <nima.doost@yahoo.com>
2025-04-17 08:38:19 -0700

    Began creating Weather App. This includes the color, spacing, information. To Do: Add days and weather in HStack

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -9,13 +9,59 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        
+        ZStack(){
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                Text("Canoga Park, CA")
+                    .font(.system(size: 30, weight: .bold))
+                    .foregroundColor(.white)
+                    .padding()
+                
+                VStack(spacing: 5){
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                    Text("76°F")
+                        .font(.system(size: 70, weight: .bold))
+                        .foregroundStyle(.white)
+                    
+                    HStack{
+                        VStack{
+                            Text("hi")
+                        }
+                    }
+                    
+                    
+                    /*VStack(alignment: .leading){
+                    
+                    Text("Tue")
+                    Image(systemName: "cloud.sun.fill")
+                    
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 80, height: 80)
+                        .font(.system(size: 22, weight: .medium))
+                        .foregroundColor(.white)
+                    
+                    
+                } */
+                    
+                    
+                }
+                Spacer()
+                    
+            }
         }
-        .padding()
     }
 }
 
